346c5801000ae71555d9be40fbb79b499601203b
hari prasad <mhariprasad94@gmail.com>
2026-02-27 10:26:58 +0530

    chore: version bump to +45 to force new update check

--- lib/providers/accounting_provider.dart
+++ lib/providers/accounting_provider.dart
@@ -37,7 +37,7 @@ class AccountingProvider with ChangeNotifier {
   double _costCenterRealBalance = 0;
   DateTime _lastSync = DateTime.now();
   bool _isSyncing = false;
-  static const String appVersion = '1.1.11+44';
+  static const String appVersion = '1.1.11+45';
 
   List<CostCenter> get costCenters => _costCenters;
   String? get activeCostCenterId => _activeCostCenterId;
