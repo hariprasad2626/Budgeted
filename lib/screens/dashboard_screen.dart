82bdbc7c2d728ca80862cfc7f2cce0252f568aee
hari prasad <mhariprasad94@gmail.com>
2026-02-27 17:33:07 +0530

    Update build date in dashboard for v1.1.14 deployment

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -616,7 +616,7 @@ class _DashboardScreenState extends State<DashboardScreen> with WidgetsBindingOb
           const Padding(
             padding: EdgeInsets.all(16.0),
             child: Text(
-              'App Version: ${AccountingProvider.appVersion}\nBuild: Feb 27, 14:55',
+              'App Version: ${AccountingProvider.appVersion}\nBuild: Feb 27, 17:30',
               style: TextStyle(color: Colors.grey, fontSize: 10),
               textAlign: TextAlign.center,
             ),
