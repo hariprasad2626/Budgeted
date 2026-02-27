ebceabc5d36c950385a3325faefdb3639c34c27d
hari prasad <mhariprasad94@gmail.com>
2026-02-27 16:34:16 +0530

    Bump cache version to force load web icons

--- build_web_log.bat
+++ build_web_log.bat
@@ -1,1 +1,1 @@
-call C:\Users\Anil\Downloads\flutter_windows_3.38.9-stable\flutter\bin\flutter.bat build web --release > build_log.txt 2>&1
+call C:\Users\Anil\Downloads\flutter_windows_3.38.9-stable\flutter\bin\flutter.bat build web --release --no-tree-shake-icons > build_log.txt 2>&1
